608542dffe25cd5384d5ebeb18a16bae9865455d
Sujit K Singh <sujit.cse.nitt@gmail.com>
2017-10-29 13:14:19 -0700

    Initial commit

---
+++ src/main/java/com/cn/thrivecart/CnThrivecartPocApplication.java
@@ -0,0 +1,12 @@
+package com.cn.thrivecart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnThrivecartPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CnThrivecartPocApplication.class, args);
+	}
+}
